ca466017c0e9e2fd6a973b976ba244875fd36951
Omar-Khc <omarabubrayo6@gmail.com>
2025-02-14 10:51:31 +0200

    Add project files.

---
+++ FreeBooks-SkillUp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FreeBooks_SkillUp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
